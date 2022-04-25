a71b044906c4f9f95dafd7c62fe7ee9ba17c3548
MS-Cinti <szabadicintia@gmail.com>
2022-04-25 10:53:57 +0200

    added map

--- frontend/script.js
+++ frontend/script.js
@@ -1,13 +1,22 @@
+const { listenerCount } = require("process");
+
 function loadEvent() {
     console.log("Az oldal betöltődött");
 
     const list = document.querySelectorAll("section");
 
-    for (const elem of list) {
+    /* for (const elem of list) {
         elem.insertAdjacentHTML("beforeend", `
         <p>text</p>
         `);
         //elem.querySelector("p").style.fontFamily = `'Poppins', sans-serif`;
-    }
+    } */
+
+    //for helyett map():
+    list.map(elem => {
+        elem.insertAdjacentHTML("beforeend", `
+        <p>text</p>
+        `);
+    })
 }
 window.addEventListener("load", loadEvent);
